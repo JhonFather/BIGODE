3772e467047b5942691679b45931e121c9f03c89
JhonFather <long.joferrary@gmail.com>
2019-06-05 00:17:03 -0300

    suave

---
+++ terceira_sprint/Site_V13/public/db/js/demo/chart-line.js
@@ -0,0 +1,296 @@
+var exibiu_graficoTemp = false;
+var exibiu_graficoUmid = false;
+
+
+
+function atualizarGrafico() {
+    obterDadosGrafico();
+    setTimeout(atualizarGrafico, 10000);
+}
+
+// altere aqui as configurações do gráfico
+// (tamanhos, cores, textos, etc)
+function configurarGraficoTemp() {
+    var configuracoes = {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: exibiu_graficoTemp ? false : { duration: 1500 },
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+            display: false,
+            text: 'Histórico recente de temperatura e umidade'
+        },
+        legend: {
+            display: false
+          },
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            intersect: false,
+            mode: 'index',
+            caretPadding: 10,
+            callbacks: {
+              label: function(tooltipItem, chart) {
+                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                return datasetLabel + " " + number_format(tooltipItem.yLabel) + ' C°';
+              }
+            }
+          },
+        layout: {
+            padding: {
+              left: 10,
+              right: 25,
+              top: 25,
+              bottom: 0
+            }
+          },
+        scales: {
+            xAxes: [{
+                time: {
+                  unit: 'time'
+                },
+                gridLines: {
+                  display: true,
+                  drawBorder: false
+                },
+                ticks: {
+                  maxTicksLimit: 7
+                }
+              }],
+            yAxes: [{
+                ticks: {
+                    maxTicksLimit: 10,
+                    padding: 10,
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                      return  number_format(value) + '°C' ;
+                    }
+                  },
+                  gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                  },
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'right',
+                id: 'y-temperatura',
+            }],       
+        }
+    };
+
+    exibiu_graficoTemp = true;
+
+    return configuracoes;
+}
+
+function configurarGraficoUmid() {
+    var configuracoes = {
+        maintainAspectRatio: false,
+        responsive: true,
+        animation: exibiu_graficoUmid ? false : { duration: 1500 },
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+            display: false,
+            text: 'Histórico recente de temperatura e umidade'
+        },
+        legend: {
+            display: false
+          },
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            intersect: false,
+            mode: 'index',
+            caretPadding: 10,
+            callbacks: {
+              label: function(tooltipItem, chart) {
+                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                return datasetLabel + " " + number_format(tooltipItem.yLabel) + ' %';
+              }
+            }
+          },
+        layout: {
+            padding: {
+              left: 10,
+              right: 25,
+              top: 25,
+              bottom: 0
+            }
+          },
+          scales: {
+            xAxes: [{
+                time: {
+                  unit: 'time'
+                },
+                gridLines: {
+                  display: true,
+                  drawBorder: false
+                },
+                ticks: {
+                  maxTicksLimit: 7
+                }
+              }],
+            yAxes: [{
+                ticks: {
+                    maxTicksLimit: 10,
+                    padding: 10,
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                      return  number_format(value) + '%' ;
+                    }
+                  },
+                  // grid line settings
+                  gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                  },
+                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: 'right',
+                    id: 'y-umidade',                                   
+            }],       
+        }
+        
+    };
+
+    exibiu_graficoUmid = true;
+
+    return configuracoes;
+}
+
+// altere aqui como os dados serão exibidos
+// e como são recuperados do BackEnd
+function obterDadosGrafico() {
+
+    // neste JSON tem que ser 'labels', 'datasets' etc, 
+    // porque é o padrão do Chart.js
+    var dadostemp = {
+        labels: [],
+        datasets: [
+            {
+                yAxisID: 'y-temperatura',
+                label: 'Temperatura',
+                lineTension: 0.3,
+                backgroundColor: "rgba(231, 74, 59, 0.0)",
+                borderColor: "rgba(231, 74, 59, 1)",
+                pointRadius: 3,
+                pointBackgroundColor: "rgba(231, 74, 59, 1)",
+                pointBorderColor: "rgba(231, 74, 59, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(231, 74, 59, 1)",
+                pointHoverBorderColor: "rgba(231, 74, 59, 1)",
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: []
+            }
+            
+        ]
+    };
+    var dadosumid = {
+        labels:[],
+        datasets:[
+            {
+                yAxisID: 'y-umidade',
+                label: 'Umidade',
+                lineTension: 0.3,
+                backgroundColor: "rgba(78, 115, 223, 0.0)",
+                borderColor: "rgba(78, 115, 223, 1)",
+                pointRadius: 3,
+                pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointBorderColor: "rgba(78, 115, 223, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: []
+            }
+        ]
+    }
+
+    fetch('/leituras/ultimas', { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+                resposta.reverse();
+
+                for (i = 0; i < resposta.length; i++) {
+                    var registro = resposta[i];
+                    atualizarUmidade(registro.umidade)
+                    atualizarTemperatura(registro.temperatura)
+                    // aqui, após 'registro.' use os nomes 
+                    // dos atributos que vem no JSON 
+                    // que gerou na consulta ao banco de dados
+
+                    dadostemp.labels.push(registro.datahora);
+                    dadostemp.datasets[0].data.push(registro.temperatura);
+               
+                    dadosumid.labels.push(registro.datahora);
+                    dadosumid.datasets[0].data.push(registro.umidade);
+                }
+                console.log(JSON.stringify(dadostemp, dadosumid));
+
+                div_aguarde.style.display = 'none';
+                div_aguarde2.style.display = 'none';
+
+                plotarGrafico(dadostemp, dadosumid);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+
+}
+function atualizarTemperatura(temperatura) {
+    dado_temp.innerHTML = `${temperatura}°C` ;
+}
+
+function atualizarUmidade(umidade) {
+    dado_umid.innerHTML = `${umidade}%`;
+}
+// só altere aqui se souber o que está fazendo!
+function plotarGrafico(dadostemp, dadosumid) {
+    console.log('iniciando plotagem do gráfico...');
+
+    var ctx = canvas_temperatura.getContext('2d');
+    window.grafico_linha = Chart.Line(ctx, {
+        data: dadostemp,
+        options: configurarGraficoTemp()
+        
+    });
+    var ctx2 = canvas_umidade.getContext('2d');
+    window.grafico_linha = Chart.Line(ctx2, {
+        data: dadosumid,
+        options: configurarGraficoUmid()
+        
+    });
+}
---
+++ terceira_sprint/Site_V13/routes/leituras.js
@@ -0,0 +1,71 @@
+// não mexa nestas 3 linhas!
+var express = require('express');
+var router = express.Router();
+var banco = require('../app-banco');
+// não mexa nessas 3 linhas!
+
+router.get('/ultimas', function (req, res, next) {
+  console.log(banco.conexao);
+  banco.conectar().then(() => {
+    var limite_linhas = 8;
+    return banco.sql.query(`select top ${limite_linhas} 
+                            temperatura, 
+                            umidade, 
+                            FORMAT(datahora AT TIME ZONE 'UTC' AT TIME ZONE 'E. South America Standard Time','HH:mm:ss') as datahora 
+                            from Dados order by idDados desc`);
+  }).then(consulta => {
+
+    console.log(`Resultado da consulta: ${consulta.recordset}`);
+    res.send(consulta.recordset);
+
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+router.get('/estatisticas', function (req, res, next) {
+  console.log(banco.conexao);
+
+  var estatisticas = {
+    temp_maxima: 0, 
+    temp_minima: 0, 
+    temp_media: 0
+  };
+
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select 
+          max(temperatura) as temp_maxima, 
+          min(temperatura) as temp_minima, 
+          avg(temperatura) as temp_media 
+        from leitura
+        `);
+  }).then(consulta => {
+    estatisticas.temp_maxima = consulta.recordset[0].temp_maxima;
+    estatisticas.temp_minima = consulta.recordset[0].temp_minima;
+    estatisticas.temp_media = consulta.recordset[0].temp_media;
+    console.log(`Estatísticas: ${estatisticas}`);
+    res.send(estatisticas);
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+// não mexa nesta linha!
+module.exports = router;
